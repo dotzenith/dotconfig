9716bb0b90ac40b6bf7c8997afe7fe9f0b1fd4b6
ZenithDS <sharmadanshu01@gmail.com>
2022-01-07 22:22:45 -0500

    added new images and a few minor changes

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -38,4 +38,3 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set fillchars-=vert:\| | set fillchars+=vert:\ ]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
