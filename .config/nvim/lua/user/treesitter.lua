f0b425a897eaa9b4bbca1c0e99e241d0d381aeae
ZenithDS <sharmadanshu01@gmail.com>
2022-01-16 18:29:05 -0500

    some changes to the treesitter

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -4,7 +4,18 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {"html",
+                      "css",
+                      "c",
+                      "bash",
+                      "javascript",
+                      "latex",
+                      "lua",
+                      "regex",
+                      "rust",
+                      "python",
+                      "json",
+                      "yaml"},
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
