ced726f8398e8974a75767c0eddeabe74f3305fe
ZenithDS <sharmadanshu01@gmail.com>
2022-02-04 08:38:22 -0500

    added spicetify and minor changes to cmp.nvim

--- .config/nvim/lua/user/plugins/cmp.lua
+++ .config/nvim/lua/user/plugins/cmp.lua
@@ -58,7 +58,7 @@ cmp.setup {
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ["<C-e>"] = cmp.mapping {
+    ["<C-l>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
@@ -123,7 +123,7 @@ cmp.setup {
     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
   },
   experimental = {
-    ghost_text = false,
+    ghost_text = true,
     native_menu = false,
   },
 }
