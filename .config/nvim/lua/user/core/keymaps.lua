c6662db80a7f02cba8f26ba3f39e9db22f225fef
ZenithDS <sharmadanshu01@gmail.com>
2022-03-02 17:01:53 -0500

    no arrow keys

--- .config/nvim/lua/user/core/keymaps.lua
+++ .config/nvim/lua/user/core/keymaps.lua
@@ -61,10 +61,16 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
--- Terminal --
--- Better terminal navigation
+-- No arrow keys 
+vim.cmd [[
+  noremap <Up> <Nop>
+  noremap <Down> <Nop>
+  noremap <Left> <Nop>
+  noremap <Right> <Nop>
+]]
+
+-- Terminal -- Better terminal navigation
 -- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
