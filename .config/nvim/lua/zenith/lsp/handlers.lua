4013d2afd76a7d0f5541f5fa5b8286eda1961ca3
ZenithDS <sharmadanshu01@gmail.com>
2022-04-19 00:46:40 -0400

    remove tsserver from handlers

--- .config/nvim/lua/zenith/lsp/handlers.lua
+++ .config/nvim/lua/zenith/lsp/handlers.lua
@@ -75,9 +75,6 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
-  end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
