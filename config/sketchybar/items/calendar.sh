b4268f549313dcb57dcb7ecfc6791385b8a7810a
dotzenith <sharmadanshu01@gmail.com>
2022-07-28 01:15:44 -0400

    it's sketchy time

---
+++ config/sketchybar/items/calendar.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+sketchybar --add item     calendar right               \
+           --set calendar update_freq=15               \
+                          icon=cal                     \
+                          icon.color=$BLACK            \
+                          icon.font="$FONT:Black:12.0" \
+                          icon.padding_left=15         \
+                          label.width=55               \
+                          label.align=right            \
+                          label.color=$BLACK           \
+                          label.padding_right=15       \
+                          align=center                 \
+                          script="$PLUGIN_DIR/time.sh" \
+                          background.color=0xffb8c0e0  \
+                          background.height=26         \
+                          background.corner_radius=5
---
+++ config/sketchybar/plugins/time.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$(date '+%H:%M')"
+
