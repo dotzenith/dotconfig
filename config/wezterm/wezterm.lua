e45e657eeb4d64cc183acfa2428824b825356d88
dotzenith <sharmadanshu01@gmail.com>
2022-08-30 11:56:21 -0400

    padding

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -23,6 +23,12 @@ return {
 		split = "#161320",
 		visual_bell = "#302D41",
 	},
+  window_padding = {
+    left = 15,
+    right = 15,
+    top = 10,
+    bottom = 0,
+  },
   
   window_background_opacity = 0.92,
 
