76967933bcc8fe838ce2ed0b3af42acdc0ffa9ab
dotzenith <sharmadanshu01@gmail.com>
2022-10-04 15:13:23 -0400

    nvim patch?

--- config/nvim/lua/zenith/lsp/handlers.lua
+++ config/nvim/lua/zenith/lsp/handlers.lua
@@ -45,7 +45,7 @@ end
 
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
+  if client.server_capabilities.document_highlight then
     vim.api.nvim_exec(
       [[
       augroup lsp_document_highlight
