c4507a449000eb1c9424da2a8048f4576bd1b102
dotzenith <sharmadanshu01@gmail.com>
2023-01-16 15:35:10 -0500

    relative numbers

--- config/nvim/lua/core/autocommands.lua
+++ config/nvim/lua/core/autocommands.lua
@@ -32,3 +32,23 @@ autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
+
+autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+   pattern = "*",
+   callback = function()
+      if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+         vim.opt.relativenumber = true
+      end
+   end,
+})
+
+autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+   pattern = "*",
+   group = augroup,
+   callback = function()
+      if vim.o.nu then
+         vim.opt.relativenumber = false
+         vim.cmd "redraw"
+      end
+   end,
+})
