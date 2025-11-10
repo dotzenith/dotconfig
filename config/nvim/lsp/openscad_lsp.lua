0799f2446cf17e7af9d38c5e31f9ec221216bd32
dotzenith <sharmadanshu01@gmail.com>
2025-11-10 17:24:20 -0500

    general housekeeping updates

--- config/nvim/lsp/openscad_lsp.lua
+++ config/nvim/lsp/openscad_lsp.lua
@@ -2,4 +2,10 @@ return {
   cmd = { 'openscad-lsp', '--stdio' },
   filetypes = { 'openscad' },
   root_markers = { '.git' },
+
+  settings = {
+    openscad =  {
+      search_paths = "/Users/sharmadanshu/Documents/OpenSCAD/libraries/",
+    }
+  }
 }
